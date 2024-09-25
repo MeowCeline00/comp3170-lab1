d80e2f46cb961bb0eba1fadd1eeabe13fc73c2c3
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-09-24 21:52:00 -0700

    adding lab-2 content to this assi branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import React from 'react';
 import { artists } from './best-selling-music-artists';
 
 function App() {
-
   return (
     <div className="app">
-      
+      <header>
+        <h1>Best-selling music artists</h1>
+      </header>
+      <div>
+        {artists.map(artist => (
+          <ArtistInfo key={artist.name} artist={artist} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ArtistInfo({ artist }) {
+  return (
+    <div className="artist-card">
+      <div className="artist-image">
+        <img src={artist.photo_url} alt={artist.name} />
+      </div>
+      <div className="artist-details">
+        <h2>{artist.name}</h2>
+        <p>Country: {artist.country}</p>
+        <p>Years active: {artist.years_active}</p>
+      </div>
     </div>
   );
 }
