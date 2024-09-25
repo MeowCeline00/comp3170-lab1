542125c63af8e688493dc4bd7732ba9c4bc52dd7
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-09-24 23:02:33 -0700

    fix code

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/comp3170-lab1/lab-2-assignment/' 
+  base: '/comp3170-lab1/lab-2-assignment/'  
+})
